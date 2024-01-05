9b248bc1e3a46e1ae6c79e2433ee062aa8374787
zero <zero.kwok@foxmail.com>
2024-01-05 15:12:21 +0800

    增加文件签名校验

--- example/example.cpp
+++ example/example.cpp
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include "downloader.h"
 #include "common/assert.hpp"
+#include "common/digest.hpp"
 #include "platform/console_win.h"
 #include "filesystem/path_util.h"
 
@@ -51,8 +52,11 @@ int main()
     auto file3 = "http://localhost:3000/download/setup.exe";
     auto file4 = "http://localhost:3000/download2/setup.exe";
     auto file5 = "http://www2.aomeisoftware.com/download/ftl/FoneTool_free.exe";
+    auto file6 = "http://192.168.3.104:8000/FoneTool_install.exe";
+    auto file7 = "https://www2.aomeisoftware.com/download/testing-env/FoneTool_free.exe";
+    auto file8 = "http://192.168.4.199/FTpackage/FoneTool_setup.exe";
 
-    auto url = file5;
+    auto url = file8;
     std::filesystem::path directory = LR"(L:\Temp)";
     std::filesystem::path file = directory / util::path_find_filename(url);
 
@@ -71,7 +75,24 @@ int main()
     if (ecode)
         std::cout << "download_file() failed, error: " << ecode.message() << std::endl;
     else
+    {
         std::cout << "download_file() filished!" << std::endl;
+
+        auto pos1 = util::win::cursor_pos();
+        auto block = 1024 * 512;
+        auto digest = util::file_sha1_digest(file, block,
+            [=](util::fsize processed, util::fsize size) -> bool {
+                if (processed % (block * 4) == 0) {
+                    util::win::cursor_goto(pos1);
+                    util::win::output_progress(processed * 100.0 / size);
+                }
+                return gFlags == kFlagRunning;
+            });
+
+        std::cout << std::endl;
+        std::cout << "file_sha1_digest() -> " << util::bytes_into_hex(digest) << std::endl;
+    }
+
     ::system("pause");
 
     return 0;
