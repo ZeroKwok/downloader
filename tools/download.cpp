d6dc38b94b5f589d5ff5572fe7bf0939ab112a01
zero <zero.kwok@foxmail.com>
2024-07-08 10:57:29 +0800

    优化 Tools 输出信息

--- tools/download.cpp
+++ tools/download.cpp
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include <conio.h>
 #include "downloader.h"
+#include "common/unit.h"
 #include "common/assert.hpp"
 #include "common/digest.hpp"
 #include "platform/console_win.h"
@@ -34,10 +35,14 @@ void BreakCallback(int signum)
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        std::cerr << "Using download.exe <url> [file] [timeout] [connections]" << std::endl;
-        return -2;
-    }
+    auto showHelp = []()
+        {
+            std::cerr << "Using download.exe <url> [file] [timeout-ms] [connections]" << std::endl;
+            return -2;
+        };
+
+    if (argc < 2)
+        return showHelp();
 
     NLOG_CFG cfg = {
         util::path_from_temp("DownloadLogs"),
@@ -57,46 +62,55 @@ int main(int argc, char** argv)
     {
         char* tail = nullptr;
         preference.timeout = strtoull(argv[3], &tail, 10);
-        if (tail && (tail[0] != '\0')) {
-            std::cerr << "Using download.exe <url> [file] [timeout] [connections]" << std::endl;
-            return -2;
-        }
+        if (tail && (tail[0] != '\0'))
+            return showHelp();
     }
 
     if (argc > 4)
     {
         char* tail = nullptr;
         preference.connections = strtoull(argv[4], &tail, 10);
-        if (tail && (tail[0] != '\0')) {
-            std::cerr << "Using download.exe <url> [file] [timeout] [connections]" << std::endl;
-            return -2;
-        }
+        if (tail && (tail[0] != '\0'))
+            return showHelp();
     }
 
     //_getch();
     NLOG_APP(" download.exe argc: %d", argc);
     NLOG_APP(" - URL: ") << url;
     NLOG_APP(" - File: ") << file;
-    NLOG_APP(" - Timeout: ") << preference.timeout;
+    NLOG_APP(" - Timeout(MS): ") << preference.timeout;
     NLOG_APP(" - Connections: ") << preference.connections;
 
+    std::cout << "Downloading ..." << std::endl;
+
     auto pos = util::win::cursor_pos();
-    DownloadFile(url, file, [=](const download_status& status)->bool
+    auto start = std::chrono::steady_clock::now();
+    auto laset = 0.0;
+    DownloadFile(url, file, [&](const download_status& status)->bool
         {
-            util::win::cursor_goto(pos);
-            util::win::output_progress(status.processedBytes * 100.0 / status.totalBytes);
+            auto p = status.processedBytes * 100.0 / status.totalBytes;
+            if (p - laset > 0.1) {
+                laset = p;
+                util::win::cursor_goto(pos);
+                util::win::output_progress(p);
+            }
             return gFlags == kFlagRunning;
         },
         preference, ecode);
+    auto end = std::chrono::steady_clock::now();
+    auto elapse = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-    std::cout << std::endl;
     if (ecode)
     {
-        std::cerr << "Download failed, error: " << ecode.message() << std::endl;
+        std::cerr << "Download failed, elapse: " 
+            << util::duration_ms_format(elapse) 
+            << ", error: " << ecode.message();
     }
     else
     {
-        std::cout << "Download finished!" << std::endl;
+        std::cout << "Download finished, elapse: " 
+            << util::duration_ms_format(elapse)
+            << std::endl;
 
         auto pos1 = util::win::cursor_pos();
         auto block = 1024 * 512;
